d0a2e2247420a3d86d64c17c1bc6ef3ffc7f75ea
DevER-M <sankam.mithunganeshviib@gmail.com>
2022-01-25 20:22:08 +0530

    commited on main

---
+++ main.py
@@ -0,0 +1,101 @@
+import os
+import sys
+
+sys.path.insert(1, './modules')
+import json
+
+import asyncio
+import discord
+import datetime
+import keep_alive
+import youtube_dl
+import discord_slash
+from os import getenv
+import impfunctions as func
+from keywords import KEYWORD
+from googlesearch import search
+from discord.ext import commands
+from youtube_dl import YoutubeDL
+from discord_slash import SlashCommand
+from discord_slash.model import ButtonStyle
+from discord_slash.utils.manage_components import create_button, create_actionrow
+from discord_slash.utils.manage_components import ComponentContext, wait_for_component
+
+bot = commands.Bot(command_prefix='.', help_command=None)
+
+slash = SlashCommand(bot,
+                     sync_commands=True,
+                     sync_on_cog_reload=True,
+                     application_id=916685474364534805)
+
+beats_activity = [discord.ActivityType.listening, "Beats"]
+
+extensions = ["music", "general"]
+bot.load_extension("music")
+bot.load_extension("general")
+bot.load_extension("slashcog")
+
+
+
+@bot.event
+#on ready
+async def on_ready():
+    print("logged in as {0.user}".format(bot))
+    await bot.change_presence(activity=discord.Activity(
+        type=beats_activity[0], name=beats_activity[1]),
+                              status=discord.Status.dnd)
+
+
+
+
+
+@bot.command()
+async def gs(ctx,*, query):
+    await ctx.author.send(f"Here are the links related to your question!")
+    for j in search(query, safe='on', start=1, stop=1):
+        await ctx.author.send(f"\n:point_right: {j}")
+        await ctx.author.send(
+            "Have any more questions:question:\nFeel free to ask again :smiley: !"
+        )
+
+
+@bot.command()
+async def servers(ctx):
+    await ctx.send(
+        embed=discord.Embed(description=f"`i am in {len(bot.guilds)} guilds`",
+                            color=discord.Color.green()))
+
+
+@bot.command()
+async def rawserv(ctx):
+    await ctx.send(bot.guilds)
+
+@bot.command()
+async def servname(ctx):
+    names = []
+    for guild in bot.guilds:names.append(f"**`{guild.name}`**")
+    await ctx.send(embed=discord.Embed(description=f"\n".join(names),color=discord.Color.green()))
+
+
+NEXTBUTTON = [
+    create_button(ButtonStyle.green, label="Next", custom_id="NextMeme")
+]
+
+
+@bot.command(name="meme")
+async def meme_(ctx):
+    em = func.meme()
+    await ctx.send(embed=em, components=[create_actionrow(*NEXTBUTTON)])
+
+    while 1:
+        try:
+            button_ctx: ComponentContext = await wait_for_component(
+                bot, components=NEXTBUTTON, timeout=10)
+            await button_ctx.edit_origin(embed=func.meme())
+        except asyncio.exceptions.TimeoutError:
+            break
+
+
+TOKEN = str(input("token: "))
+keep_alive.keep_alive()
+bot.run(TOKEN)  # client login
---
+++ modules/impfunctions.py
@@ -0,0 +1,141 @@
+import random
+import requests
+import os
+import requests as req
+import discord
+import discord.ext
+from bs4 import BeautifulSoup
+from cryptography.fernet import Fernet
+
+DOGAPI = "http://thedogapi.com/api/images/get.php"
+CATAPI = "http://thecatapi.com/api/images/get.php"
+
+
+def roll_dice():
+    return random.randint(1, 6)
+
+
+def getCatPicture():
+    catPicture = requests.get(CATAPI)
+    if catPicture.status_code == 200:
+        catPicture = catPicture.url
+        return catPicture
+
+
+def getDogPicture():
+    catPicture = requests.get(DOGAPI)
+    if catPicture.status_code == 200:
+        catPicture = catPicture.url
+        return catPicture
+
+
+def help():
+
+    embed = discord.Embed(title="COMMANDS :", colour=discord.Colour(0x3f2f2f))
+    embed.set_thumbnail(url="https://c.tenor.com/bC95JP5b-a0AAAAi/tegan-iversen-socks.gif")
+    embed.set_author(name="sockey",icon_url="https://c.tenor.com/bC95JP5b-a0AAAAi/tegan-iversen-socks.gif")
+    embed.add_field(name=".ping", value="just returns pong", inline=True)
+    embed.add_field(name=".cat", value="sends cute cat pictures", inline=True)
+    embed.add_field(name=".dog", value="sends a cute dog picure", inline=True)
+    embed.add_field(name=".dice",
+                    value="returns a random number between 1 to 6",
+                    inline=True)
+    embed.add_field(name=".help",
+                    value="shows this embed                                ",
+                    inline=True)
+    embed.add_field(name=".meme", value="sends a meme", inline=True)
+    embed.add_field(name=".math",
+                    value="""*adds,subtracts,multiplies or divides* 
+       eg: $math 35+33 'the ans would be 68'""",
+                    inline=False)
+    embed.add_field(name=".rnum",
+                    value="sends a randum number from given parameters ",
+                    inline=True)
+    embed.add_field(name=".play `<search query or link>`",
+                    value="plays music!",
+                    inline=True)
+    embed.add_field(name=".join", value="joins a channel", inline=True)
+    embed.add_field(name=".skip",
+                    value="skips the currnet playing song",
+                    inline=True)
+    embed.add_field(name=".q", value="shows the queue", inline=True)
+    embed.add_field(name=".pause",
+                    value="you should understand this",
+                    inline=True)
+    embed.add_field(name=".resume",
+                    value="you should understand this too!!",
+                    inline=True)
+    embed.add_field(name=".encode `string`",
+                    value="encode the string and sends it to you",
+                    inline=True)
+    embed.add_field(name=".decode `encoded tring`",
+                    value="decode the string and sends it to you",
+                    inline=True)
+    embed.add_field(name=".lofi", value="plays lofi", inline=True)
+    embed.add_field(name=".fa `song query`",
+                    value="adds songs to a private playlist",
+                    inline=True)
+    embed.add_field(name=".fv",
+                    value="shows the private playlist",
+                    inline=True)
+    embed.add_field(name=".fd `index`",
+                    value="removes the given index from playlist",
+                    inline=True)
+    embed.add_field(name=".gs `query`",
+                    value="searches google and send you the results",
+                    inline=True)
+
+    return embed
+
+
+def meme():
+    response = requests.get("https://memes.blademaker.tv/api?lang=en")
+    jsonresponse = response.json()
+
+    title = jsonresponse["title"]
+    ups = jsonresponse["ups"]
+    image = jsonresponse["image"]
+    downs = jsonresponse["downs"]
+    embed = discord.Embed(title=f"{title}")
+    embed.set_image(url=image)
+    embed.set_footer(text=f"👍: {ups} 👎: {downs}")
+    return embed
+
+
+def math_eval(arg):
+    try:
+        response = eval(arg)
+        return response
+
+    except:
+        response = "some weird error occured"
+        return response
+
+
+def randnum(a, b):
+    return str(random.randint(a, b))
+
+
+def encrypt(string: str):
+    key = Fernet.generate_key()
+    f = Fernet(key)
+    string = string.encode()
+    token = f.encrypt(string)
+    return token, key
+
+
+def decrypt(token: str, key: str):
+    key = key.encode()
+    f = Fernet(key)
+    return f.decrypt(token)
+
+
+def Getyt(url: str):
+    r = req.get(url, "html.parser")
+
+    soup = BeautifulSoup(r.text, "html.parser")
+    vidtitle = str(soup.find("title").text)
+    vidtitle = vidtitle.replace(" - YouTube", "")
+    return vidtitle
+
+

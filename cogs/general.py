d23539ad87bbbc10ca63320c16e1ea8a123b6f0b
DevER-M <sankam.mithunganeshviib@gmail.com>
2022-05-05 20:37:09 +0530

    small tweaks

--- cogs/general.py
+++ cogs/general.py
@@ -21,18 +21,6 @@ CATAPI = "http://thecatapi.com/api/images/get.php"
 
 class General(commands.Cog):
     emoji="🔖"
-    '''
-        show the ping of the bot from the user `.ping`\n
-        it rolls dice and sends the random number from 1 to 6 `.dice`\n
-        sends a cute picture of a cat `.cat`\n
-        sends a cute picture of a dog `.dog`\n
-        sends a random number between the given numbers `.rnum <min> <max>`\n
-        encodes a string to base64 `.encode <string>`\n
-        decodes encoded string back to how it used to be `.decode <encoded string>`\n
-        sends some nice memes `.meme`\n
-        shows the number servers the bot is in `.servers`\n
-        #googleit `.gs <query>`\n
-    '''
     
     __slots__ = ('bot')
 
@@ -43,18 +31,14 @@ class General(commands.Cog):
 
     @commands.command(name="ping")
     async def ping_(self, ctx):
-        """!
-        show the ping of the bot from the user `.ping`
-        """
+        """show the ping of the bot from the user """
         async with ctx.typing():
             await asyncio.sleep(0.2)
         await ctx.send(f"pong {round(self.bot.latency*1000)} ms")
 
     @commands.command(name="dice")
     async def dice_(self, ctx):
-        """!
-        it rolls dice and sends the random number from 1 to 6 `.dice`
-        """
+        """ it rolls dice and sends the random number from 1 to 6"""
         async with ctx.typing():
             await asyncio.sleep(0.2)
         rand_num = func.roll_dice()
@@ -62,33 +46,25 @@ class General(commands.Cog):
 
     @commands.command(name="cat")
     async def cat_(self, ctx):
-        """!
-        sends a cute picture of a cat `.cat`
-        """
+        """sends a cute picture of a cat"""
         await ctx.send(func.getCatPicture())
 
     @commands.command(name="dog")
     #dog pic command
     async def dog_(self, ctx):
-        """!
-        sends a cure picture of a dog `.dog`
-        """
+        """sends a cure picture of a dog"""
         await ctx.send(func.getDogPicture())
     
 
     @commands.command(name="rnum")
     async def rnum_(self,ctx,a: int,b: int,):
-        """!
-        sends a random number between the given numbers `.rnum <min> <max>`
-        """
+        """sends a random number between the given numbers"""
         await ctx.send(func.randnum(a=a, b=b))
 
 
     @commands.command(description="encodes a string to base64 `.encode <string>`")
     async def encode(self, ctx, message_toencode):
-        """!
-        encodes a string to base64 `.encode <string>`
-        """
+        """encodes a string to base64"""
         await ctx.message.delete()
 
         message_toencode = str(message_toencode).encode()
@@ -102,9 +78,7 @@ class General(commands.Cog):
 
     @commands.command(description="decodes encoded string back to how it used to be `.decode <encoded string>`")
     async def decode(self, ctx, message_toencode):
-        """!
-        decodes encoded string back to how it used to be `.decode <encoded string>`
-        """
+        """decodes encoded string back to how it used to be"""
         await ctx.message.delete()
 
         message_toencode = str(message_toencode).encode()
@@ -116,29 +90,19 @@ class General(commands.Cog):
 
         await ctx.author.send(f"decoded string: {message_toencode}")
 
-    """
+    
     @commands.command(name="meme",description="sends some nice memes `.meme`")
     async def meme_(self,ctx):
-        
-        sends some nice memes `.meme`
+        """sends some nice memes """
         
         em = func.meme()
-        await ctx.send(embed=em, components=[create_actionrow(*NEXTBUTTON)])
+        await ctx.send(embed=em,)
 
-        while 1:
-            try:
-                button_ctx: ComponentContext = await wait_for_component(
-                self.bot, components=NEXTBUTTON, timeout=10)
+ 
 
-                await button_ctx.edit_origin(embed=func.meme())
-            except asyncio.exceptions.TimeoutError:
-                break
-    """
     @commands.command(name="gs")
     async def gs(self, ctx, *, query):
-        """!
-        #googleit `.gs <query>`
-        # """
+        """#googleit"""
         await ctx.author.send(f"Here are the links related to your question!")
         for j in search(query, safe='on', start=1, stop=1):
             await ctx.author.send(f"\n:point_right: {j}")
@@ -146,9 +110,7 @@ class General(commands.Cog):
     
     @commands.command(name="servers")
     async def servers_(self,ctx):
-        """!
-        shows the number servers the bot is in `.servers`
-        """
+        """shows the number servers the bot is in"""
         names = []
         for guild in self.bot.guilds:names.append(f"**`{guild.name}`**")
         await ctx.send(f"**guilds: {len(self.bot.guilds)}**",embed=discord.Embed(description=f"\n".join(names),color=discord.Color.green()))

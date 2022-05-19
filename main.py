5b08d35763e9313f461ca1f093ae413c1f1dfc39
DevER-M <sankam.mithunganeshviib@gmail.com>
2022-05-19 19:45:33 +0530

    starting to remove slash commands

--- main.py
+++ main.py
@@ -6,15 +6,9 @@ import discord,asyncio
 import keep_alive
 from googlesearch import search
 from discord.ext import commands
-#from discord_slash import SlashCommand
-
 
 
 bot = commands.Bot(command_prefix='.',intents=discord.Intents.all())
-#slash = SlashCommand(bot,
-                    # sync_commands=True,
-                     #sync_on_cog_reload=True,
-                     #application_id=916685474364534805)
 beats_activity = [discord.ActivityType.listening, "Beats"]
 
 cogs = [
@@ -28,9 +22,8 @@ cogs = [
 @bot.event
 async def on_ready():
     print("logged in as {0.user}".format(bot))
-    await bot.change_presence(activity=discord.Activity(
-        type=beats_activity[0], name=beats_activity[1]),
-                              status=discord.Status.dnd)
+    await bot.change_presence(activity=discord.Activity(type=beats_activity[0], name=beats_activity[1]),status=discord.Status.dnd)
+
 async def load_extension():
     for file in cogs:
         await bot.load_extension(f'cogs.{file}')
@@ -38,10 +31,7 @@ async def load_extension():
 async def main():
     async with bot:
         await load_extension()
-        TOKEN = str(os.getenv('TOKEN')) or input('enter token: ')
+        TOKEN = str(os.getenv('TOKEN'))
         await bot.start(TOKEN)
 
-
-# keep_alive.keep_alive()
-# bot.run(TOKEN) #client login
 asyncio.run(main())
